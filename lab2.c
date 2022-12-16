9b0debfd323fb81cc626e45702b9a1ff4b10e93c
JessicaGPerez <75407206+JessicaGPerez@users.noreply.github.com>
2022-12-16 13:49:47 -0500

    Add files via upload

--- lab2.c
+++ lab2.c
@@ -8,11 +8,11 @@
 using namespace std;
 
 
-int** read_board_from_file(/*char* filename*/){
+int** read_board_from_file(char* filename){
     //FILE *fp = NULL;
-    string file = "valid_board.txt";
+    filename = "valid_board.txt";
     ifstream inFile;
-    inFile.open(file);
+    inFile.open(filename);
 
     string line = "";
     string temp = "";
@@ -80,15 +80,68 @@ int** read_board_from_file(/*char* filename*/){
     //return board;
 }
 
+bool notRow(char sudoku_board, int row)
+{
+    set<char> st;
+    for (int i = 0; i < 9; i++) {
+        if (st.find(sudoku_board[i][row]) != st.end())
+            return false;
+        if (sudoku_board[i][row] != '.')
+            st.insert(sudoku_board[i][row]);
+    }
+    return true;
+}
 
+bool notCol(char sudoku_board, int col)
+{
+    set<char> st;
+    for (int i = 0; i < 9; i++) {
+        if (st.find(sudoku_board[i][col]) != st.end())
+            return false;
+        if (sudoku_board[i][col] != '.')
+            st.insert(sudoku_board[i][col]);
+    }
+    return true;
+}
+
+bool notBox(char sudoku_board, int starting_row, int starting_col)
+{
+    set <char> st;
+
+    for (int row = 0; row < 3; row++){
+        for (int col = 0; col < 3; col++){
+            char current_box = arr[row + starting_row][col + starting_row];
+            if (st.find(current_box) != st.end())
+                return false;
+            if (current_box != '.')
+                st.insert(current_box);
+        }
+    }
+    return true;
+}
+bool isValidating( char  sudoku_board, int row, int col)
+{
+    return notRow(sudoku_board, row) && notCol(sudoku_board,col) && notBox(sudoku_board, row - row % 3, col - col % 3);
+}
 int is_board_valid(){
-    pthread_t* tid;  /* the thread identifiers */
-    pthread_attr_t attr;
-    param_struct* parameter;
+    int m;
+    char sudoku_board;
+    for (int r = 0; r < m; r++){
+        for (int c = 0; c < m; c++){
+            if (!isValidating(sudoku_board, r, c))
+                return 0;
+        }
+    }
+    return 1;
+}
+
+    //pthread_t* tid;  /* the thread identifiers */
+   // pthread_attr_t attr;
+    //param_struct* parameter;
 
     // replace this comment with your code
 
-}
+
 
 
 
